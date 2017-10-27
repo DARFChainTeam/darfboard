7b155ef8e5b6bbb53b1b620231ce93c14a6a0098
Sergey <stepanetssergey@gmail.com>
2017-10-27 15:10:26 +0300

    changes web3 ipfs

--- darfboard/models/setting.py
+++ darfboard/models/setting.py
@@ -40,8 +40,8 @@ class SettingOfJournals(models.Model):
     ethereum_pk = fields.Char(string="Ethereum smart contract address")   
     ethereum_node_address = fields.Char(string="Ethereum node address")
     ethereum_address = fields.Char(string="Smart Contract Interface")
-    gas_limit = fields.Float()
-    gas_spent = fields.Float()
+    gas_limit = fields.Float(string='Gas limit',compute='_gas_limit')
+    gas_spent = fields.Float(string='Gas will be spent',compute='_gas_spent')
     send_period = fields.Selection([
             ('day', 'Every day'),
             ('week', 'Weekly'),
@@ -87,16 +87,10 @@ class SettingOfJournals(models.Model):
         contract = web3.eth.contract(abi=json.loads(abi_json), address=ethereum_contract_address)
         hash_of_synchronaze = '"'+base58.b58encode(str(date_of_synchronization))+'"'
         print hash_of_synchronaze
-        if self.import_export == 'export':
-            try:
-                result_of_gas_estimate = contract.estimateGas().setData(str(hash_of_synchronaze))
-            except:
-                result_of_gas_estimate = 0
-        if self.import_export == 'import':
-            try:
-                result_of_gas_estimate = contract.estimateGas().HashOfDB()
-            except:
-                result_of_gas_estimate = 0
+        try:
+            result_of_gas_estimate = contract.estimateGas().setData(str(hash_of_synchronaze))
+        except:
+            result_of_gas_estimate = 0
         self.gas_spent = result_of_gas_estimate
     
     def sand_report(self):
@@ -119,9 +113,52 @@ class SettingOfJournals(models.Model):
             for item_journal in item_list.journal_list:
                 journal = etree.SubElement(journals,'journal')
                 journal.set("name",item_journal.name)
-            
+                if item_journal.default_debit_account_id.code:
+                    debit = etree.SubElement(journal,'debit')
+                    debit.set("code",item_journal.default_debit_account_id.code)
+                    debit.set("name",item_journal.default_debit_account_id.name)
+                for item_account_move in self.env['account.move'].search([('journal_id','=',item_journal.id)]):
+                    entry = etree.SubElement(journal, 'entry')
+                    entry.set("number",item_account_move.name)
+                    date = etree.SubElement(entry,'date')
+                    date.text = item_account_move.date
+                    for item_entry in item_account_move.line_ids:
+                        entry_item = etree.SubElement(entry,'entry_item')
+                        entry_item.set("name",item_entry.name)
+                        account_code = etree.SubElement(entry_item,'code')
+                        account_code.text = item_entry.account_id.code
+                        account_name = etree.SubElement(entry_item,'name')
+                        account_name.text = item_entry.account_id.name
+                        debit_item = etree.SubElement(entry_item,'dabit')
+                        debit_item.text = str(item_entry.debit)
+                        credit_item = etree.SubElement(entry_item,'credit')
+                        credit_item.text = str(item_entry.credit)
+                        quantity_item = etree.SubElement(entry_item,'quantity')
+                        quantity_item.text = str(item_entry.quantity)
+                        
+                        
+                    
         self.xml_for_synchronization = etree.tostring(root, pretty_print=True)
-        
+        xml_result = etree.tostring(root)
+        apifs_node_api = ipfsapi.Client('localhost',5001)
+        with io.open('tmp.json', 'w', encoding='utf-8') as f:
+            result_of_file = f.write(unicode(xml_result))
+        print "test of save ipfs"
+        res = apifs_node_api.add('tmp.json')
+        print 'Address of IPFS'
+        print res
+        result_save_address = str(res['Hash']) 
+        _logger.info('ethereum')
+        address_node = self.ethereum_node_address
+        web3 = Web3(HTTPProvider(address_node))
+        abi_json = self.ethereum_address
+        ethereum_contract_address = self.ethereum_pk
+        contract =  web3.eth.contract(abi = json.loads(abi_json), address=ethereum_contract_address)
+        hash_of_synchronaze = '"'+base58.b58encode(general_info_text)+'"'
+        print hash_of_synchronaze
+        result_save_address = '"'+result_save_address+'"'
+        #TransactionHashEthereum = contract.transact().setData(str(hash_of_synchronaze))
+        address_of_file = contract.transact().setDocumentIPFSAddress(result_save_address)
         
         
 class ListOfParameter(models.Model):
